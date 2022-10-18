6edd75eeee92f3fa95e6c99f6098e1c7cfd1990e
Brian-Machiestay <brianmachiestay@gmail.com>
2022-10-18 01:27:14 +0000

    added some js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,28 @@
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
 var shapes = ["triangle", "square", "pentagon", "circle"];
-for(var x = 0; x < shapes.length; x++) {
- console.log(shapes[x]);
+topics = ["HTML", "CSS", "JS", "GIT"]
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
+
+function listTopics() {
+    for(var x = 0; x < shapes.length; x++) {
+        console.log(shapes[x]);
+    }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('which topic should we study first');
+selectTopic();
